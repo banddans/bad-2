e9c4c1f6cfe88ca6a43eb514df7cee6d66057a40
banddans <tom.cromnow@gmail.com>
2026-07-02 12:47:37 +0200

    Display Beaches on Map

--- web/src/lib/appwrite-client.ts
+++ web/src/lib/appwrite-client.ts
@@ -1,8 +1,9 @@
-import { Client, Account, Databases } from "appwrite";
+import { Client, Account, TablesDB } from "appwrite";
 
 export const client = new Client()
   .setEndpoint("https://fra.cloud.appwrite.io/v1")
   .setProject("6a4560470013afa2043a");
 
 export const account = new Account(client);
-export const databases = new Databases(client);
+export const databases = new TablesDB(client);
+export const databaseId = "6a460878002fc6ccaaca";
--- web/src/map/Map.tsx
+++ web/src/map/Map.tsx
@@ -1,11 +1,12 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L, { LatLng } from "leaflet";
 
-// Fix för att markör-ikonen ibland inte laddas korrekt i React/Webpack
-import L from "leaflet";
+// Fix marker icon
 import markerIcon from "leaflet/dist/images/marker-icon.png";
 import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import { Button } from "../components/ui/button";
+import { useEffect, useState } from "react";
+import { databaseId, databases } from "../lib/appwrite-client";
 
 let DefaultIcon = L.icon({
   iconUrl: markerIcon,
@@ -15,15 +16,38 @@ let DefaultIcon = L.icon({
 });
 L.Marker.prototype.options.icon = DefaultIcon;
 
+interface Beach {
+  x: number;
+  y: number;
+  name: string;
+  id: string;
+}
+
 export function Map() {
-  // Koordinater för Stockholm [breddgrad, längdgrad]
-  const position = new L.LatLng(58.120948, 15.444215);
+  const [beaches, setBeaches] = useState<Beach[]>([]);
+
+  useEffect(() => {
+    const effect = async () => {
+      const newBeaches = await databases.listRows({
+        databaseId,
+        tableId: "beaches",
+      });
+
+      setBeaches(
+        newBeaches.rows.map((row) => {
+          return { x: row.x, y: row.y, name: row.name, id: row.$id };
+        }),
+      );
+    };
+
+    effect();
+  }, []);
 
   return (
     <div className="h-screen relative">
       {/* Leaflet does not like flexbox here, set fixed height instead */}
       <MapContainer
-        center={position}
+        center={new LatLng(58.41086, 15.62157)}
         zoom={13}
         style={{ height: "100%", width: "100%" }}
       >
@@ -31,10 +55,12 @@ export function Map() {
           url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
         />
-        {/* En valfri markör på kartan */}
-        <Marker position={position}>
-          <Popup>Test</Popup>
-        </Marker>
+
+        {beaches.map((beach) => (
+          <Marker position={new LatLng(beach.y, beach.x)} key={beach.id}>
+            <Popup>{beach.name}</Popup>
+          </Marker>
+        ))}
       </MapContainer>
       <div className="absolute top-0 left-0 p-2" style={{ zIndex: 500 }}></div>
     </div>
