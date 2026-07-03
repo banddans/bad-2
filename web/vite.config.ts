8674d8e6e2f55e9ba81ed816fc58bb8d66134c55
banddans <tom.cromnow@gmail.com>
2026-07-03 18:48:04 +0200

    Final Fixes Before Publishing

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,6 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  base: "/bad/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
